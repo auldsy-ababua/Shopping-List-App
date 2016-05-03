9e4d6552590ade05b5646236a0ffb269cd105662
Colin Aulds <caulds989@gmail.com>
2016-05-03 13:49:27 -0500

    newest changes

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-$(".new-list").click(function() {
+var newListCreate = function() {
     var newList = $("#list-name-input").val();
-    $(".collection").append("<li>" + newList + "</li>")
+    $(".collection").append("<li class='collection-item'>" + newList + "</li>")
+    $("#list-name-input").val('');
+};
+
+$(".new-list").click(function() {
+    newListCreate();
 });
+
+$('#list-name-input').keypress(function(e) {
+    var key = e.which;
+    if(key == 13) {
+        newListCreate();
+    }
+});
+
