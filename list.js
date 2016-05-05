967f8fbfcf5b7d5bef8771fd2203298c2fca84ad
Colin Aulds <caulds989@gmail.com>
2016-05-05 12:30:33 -0500

    Added js to list.html

--- list.js
+++ list.js
@@ -1,1 +1,16 @@
+var newItemCreate = function() {
+    var newListItem = $("#list-item-input").val();
+    $(".collection").append("<li class='collection-item'>" + newListItem + "</li>")
+    $("#list-item-input").val('');
+};
 
+$(".new-item-button").click(function() {
+    newItemCreate();
+});
+
+$('#list-item-input').keypress(function(e) {
+    var key = e.which;
+    if(key == 13) {
+        newItemCreate();
+    }
+});
