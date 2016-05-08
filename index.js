ffbdfcf2f16a04c26e099aa236b67df23d856e2d
Elisabet Vilana <Elisabet Vilana>
2016-05-08 09:47:15 +0200

    Fixed the delete button and console log the id

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var myFirebaseRef = new Firebase("https://aulds-shopping-list-app.firebaseio.com/");
 
-$(".new-list-button").click(function() {
+$(".new-list-button").click(function () {
     var fieldValue = $("#list-name-input").val();
     if (fieldValue.trim()) {
         save(fieldValue);
     }
 });
 
-$('#list-name-input').keypress(function(e) {
+$('#list-name-input').keypress(function (e) {
     var key = e.which;
-    if(key == 13) {
+    if (key == 13) {
         var fieldValue = $("#list-name-input").val();
         if (fieldValue.trim()) {
             save(fieldValue);
@@ -17,21 +17,23 @@ $('#list-name-input').keypress(function(e) {
     }
 });
 
+$('body').delegate('.deleteButton', 'click', function () {
+    console.log($(this).attr('value'));
+})
+
 function save(value) {
     $(".collection").append("<li class='collection-item'>" + value + "</li>")
     myFirebaseRef.push(value);
     $("#list-name-input").val('');
 }
+
 function load() {
-    myFirebaseRef.on("value", function(snapshot) {
-        snapshot.forEach(function(item) {
-            $(".collection").append("<li class='collection-item'>" + item.val() + "<button value='+ item.key() +'>Delete Item</button></li>")
+    myFirebaseRef.on("value", function (snapshot) {
+        snapshot.forEach(function (item) {
+            $(".collection").append("<li class='collection-item'>" + item.val() + "<button class='deleteButton' value='" + item.key() + "'>Delete Item</button></li>");
 
         });
     });
 };
-load();
 
-function deleteitem(id) {
-    console.log(id);
-}
+load();
