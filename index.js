a7f67bedf8dc82c8dacf27efd52e863dd5067b04
Colin Aulds <caulds989@gmail.com>
2016-05-08 16:06:04 -0500

    Added delete functions

--- index.js
+++ index.js
@@ -18,20 +18,32 @@ $('#list-name-input').keypress(function (e) {
 });
 
 $('body').delegate('.deleteButton', 'click', function () {
-    console.log($(this).attr('value'));
-})
+    var parent = $(this).parent();
+    console.log(parent);
+    var ref = new Firebase('https://aulds-shopping-list-app.firebaseio.com/' + $(this).attr('value'));
+    ref.remove(function(error) {
+        if (error) {
+            console.log('Synchronization failed');
+        } else {
+            parent.remove();
+        }
+    });
+});
+
+
 
 function save(value) {
     $(".collection").append("<li class='collection-item'>" + value + "</li>")
-    myFirebaseRef.push(value);
+    myFirebaseRef.push(value).then(function(item){
+        console.log(item);
+    });
     $("#list-name-input").val('');
-}
+};
 
 function load() {
-    myFirebaseRef.on("value", function (snapshot) {
+    myFirebaseRef.once("value", function (snapshot) {
         snapshot.forEach(function (item) {
             $(".collection").append("<li class='collection-item'>" + item.val() + "<button class='deleteButton' value='" + item.key() + "'>Delete Item</button></li>");
-
         });
     });
 };
